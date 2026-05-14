a5bcfc966d0c4ef6f768fbc4d7c265769ea85259
Somya Nema <somyanema18@gmail.com>
2026-05-14 22:23:59 +0530

    Fix Gemini API config

--- llm.py
+++ llm.py
@@ -1,12 +1,13 @@
 import google.generativeai as genai
+import streamlit as st
+
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+
 # 🤖 Model
 model = genai.GenerativeModel("gemini-2.5-flash")
 
